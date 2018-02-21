d05c4a40d85f46367e0f7eb4dc4c1b5f8db3a96d
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-02-21 13:07:11 -0800

    add test data model

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -10,3 +10,28 @@ const expect = chai.expect;
 const {runServer, app, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 const {BlogPost} = require('../models');
+
+
+//seed data into test db
+function seedTestDb(){
+
+}
+
+//create dummy data using faker
+function testDataModel(){
+    return
+    {
+        author: {
+            firstName: faker.name.firstName,
+            lastName: faker.name.lastName
+        },
+        title: faker.lorem.words,
+        content: faker.lorem.paragraph
+        created: faker.date.past
+    }
+}
+
+//tear down test db
+function tearDownDb(){
+
+}
