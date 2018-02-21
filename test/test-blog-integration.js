4e9e64c7918eb049b4b860e51ceeac0e3d9774b3
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-02-21 12:53:43 -0800

    add mocha test script to package.json file

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -1,5 +1,11 @@
+'use strict'
+
 const faker = require('faker');
 const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
 
 const {runServer, app, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
