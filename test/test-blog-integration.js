2500d868352c6404e392ba83a8096895cefabc48
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-02-21 12:49:53 -0800

    install chai and chai-http, and import functions into test file

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -1,2 +1,6 @@
 const faker = require('faker');
 const mongoose = require('mongoose');
+
+const {runServer, app, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+const {BlogPost} = require('../models');
